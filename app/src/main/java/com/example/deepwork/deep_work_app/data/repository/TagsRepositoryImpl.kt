283b4724a913b7e57498d54548026b23cd115ba0
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2025-01-29 10:55:06 +0300

    Added TagsRepositoryImpl.kt

---
+++ app/src/main/java/com/example/deepwork/deep_work_app/data/repository/TagsRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.example.deepwork.deep_work_app.data.repository
+
+import com.example.deepwork.deep_work_app.data.local.dao.TagsDao
+import com.example.deepwork.deep_work_app.data.local.entities.Tags
+import com.example.deepwork.deep_work_app.domain.repository.TagsRepository
+import kotlinx.coroutines.flow.Flow
+
+class TagsRepositoryImpl(
+    private val tagsDao: TagsDao
+) : TagsRepository {
+    override suspend fun insertTag(tags: Tags) {
+        tagsDao.insertTag(tags)
+    }
+
+    override suspend fun updateTag(tags: Tags) {
+        tagsDao.updateTag(tags)
+    }
+
+    override suspend fun deleteTag(tags: Tags) {
+        tagsDao.deleteTag(tags)
+    }
+
+    override fun getAllTags(): Flow<List<Tags>> {
+        return tagsDao.getAllTags()
+    }
+
+}
