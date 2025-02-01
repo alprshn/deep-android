a6ff1d9ee66d227e69964dc9a0f680f5b7d24ab2
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2025-02-01 16:30:39 +0300

    Added UserPreferences UseCases

--- app/src/main/java/com/example/deepwork/deep_work_app/domain/usecases/ChangeUserPreferencesUseCase.kt
+++ app/src/main/java/com/example/deepwork/deep_work_app/domain/usecases/ChangeUserPreferencesUseCase.kt
@@ -1,4 +1,11 @@
 package com.example.deepwork.deep_work_app.domain.usecases
 
-class ChangeUserPreferencesUseCase {
+import com.example.deepwork.deep_work_app.data.local.entities.UserPreferences
+import com.example.deepwork.deep_work_app.domain.repository.UserPreferencesRepository
+
+class ChangeUserPreferencesUseCase(private val userPreferencesRepository: UserPreferencesRepository) {
+    suspend operator fun invoke(theme: String, haptic:Boolean) {
+        val preferences = UserPreferences(theme = theme, haptic = haptic)
+        userPreferencesRepository.savePreferences(preferences)
+    }
 }
--- app/src/main/java/com/example/deepwork/deep_work_app/domain/usecases/GetUserPreferencesUseCase.kt
+++ app/src/main/java/com/example/deepwork/deep_work_app/domain/usecases/GetUserPreferencesUseCase.kt
@@ -1,4 +1,10 @@
 package com.example.deepwork.deep_work_app.domain.usecases
 
-class GetUserPreferencesUseCase {
+import com.example.deepwork.deep_work_app.data.local.entities.UserPreferences
+import com.example.deepwork.deep_work_app.domain.repository.UserPreferencesRepository
+
+class GetUserPreferencesUseCase(private val userPreferencesRepository: UserPreferencesRepository) {
+    suspend operator fun invoke(): UserPreferences? {
+        return userPreferencesRepository.getUserPreferences()
+    }
 }
