74115fb3393c6342eafb63993e06e9ac517fbec3
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2025-02-01 16:31:08 +0300

    Edit UserPreferences preferenceId

--- app/src/main/java/com/example/deepwork/deep_work_app/data/local/entities/UserPreferences.kt
+++ app/src/main/java/com/example/deepwork/deep_work_app/data/local/entities/UserPreferences.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "userPreferences_table")
 data class UserPreferences(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "preference_id") val preferenceId: Int,
+    @ColumnInfo(name = "preference_id") val preferenceId: Int = 1 ,
     @ColumnInfo(name = "theme_id") val theme: String,
     @ColumnInfo(name = "haptic") val haptic: Boolean
 )
