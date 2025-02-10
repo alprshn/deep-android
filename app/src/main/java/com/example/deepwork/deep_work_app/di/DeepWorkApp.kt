f2f3dabd1237a75d3edeb3cfca406209ad8bcaa9
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2025-02-10 17:25:23 +0300

    Added DeepWorkApp.kt for Dependency Injection

---
+++ app/src/main/java/com/example/deepwork/deep_work_app/di/DeepWorkApp.kt
@@ -0,0 +1,9 @@
+package com.example.deepwork.deep_work_app.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DeepWorkApp: Application() {
+
+}
