9348d2ca17413ab121b0c9763c73715a378ea9c6
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2025-02-10 17:47:49 +0300

    Added BottomNavigagate Items

---
+++ app/src/main/java/com/example/deepwork/deep_work_app/presentation/components/bottom_bar/BottomNavigationBar.kt
@@ -0,0 +1,9 @@
+package com.example.deepwork.deep_work_app.presentation.components.bottom_bar
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+
+}
