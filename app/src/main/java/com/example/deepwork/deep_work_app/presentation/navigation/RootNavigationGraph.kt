ad62a005277b3928b1f3762216f39dd1f941c8df
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2025-02-12 17:17:41 +0300

    Added Screens

--- app/src/main/java/com/example/deepwork/deep_work_app/presentation/navigation/RootNavigationGraph.kt
+++ app/src/main/java/com/example/deepwork/deep_work_app/presentation/navigation/RootNavigationGraph.kt
@@ -5,6 +5,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.deepwork.deep_work_app.presentation.navigation.bottom_bar.BottomBarTab
+import com.example.deepwork.deep_work_app.presentation.settings_screen.SettingsScreen
+import com.example.deepwork.deep_work_app.presentation.statistics_screen.StatisticsScreen
+import com.example.deepwork.deep_work_app.presentation.timeline_screen.TimelineScreen
+import com.example.deepwork.deep_work_app.presentation.timer_screen.TimerScreen
 
 @Composable
 fun RootNavigationGraph(navController : NavHostController){
@@ -12,10 +16,9 @@ fun RootNavigationGraph(navController : NavHostController){
         navController = navController,
         startDestination = BottomBarTab.Statistics.title
     ){
-        composable(BottomBarTab.Statistics.title){}
-        composable(BottomBarTab.Timer.title){}
-        composable(BottomBarTab.Timeline.title){}
-        composable(BottomBarTab.Settings.title){}
-
+        composable(BottomBarTab.Statistics.title){StatisticsScreen()}
+        composable(BottomBarTab.Timer.title){TimerScreen()}
+        composable(BottomBarTab.Timeline.title){TimelineScreen()}
+        composable(BottomBarTab.Settings.title){SettingsScreen()}
    }
 }
