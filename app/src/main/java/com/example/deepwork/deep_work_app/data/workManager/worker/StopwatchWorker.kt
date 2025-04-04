6616307fc7ae449f83cf1f469fc11f8a0b0d0465
alprshn <alper_sahin20@erdogan.edu.tr>
2025-04-04 15:56:14 +0300

    Add Stopwatch Worker and Stopwatch Manager

---
+++ app/src/main/java/com/example/deepwork/deep_work_app/data/workManager/worker/StopwatchWorker.kt
@@ -0,0 +1,51 @@
+package com.example.deepwork.deep_work_app.data.workManager.worker
+
+import androidx.hilt.work.HiltWorker
+import android.content.Context
+import android.content.pm.ServiceInfo
+import android.os.Build
+import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
+import androidx.work.WorkerParameters
+import com.example.deepwork.deep_work_app.data.manager.StopwatchManager
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.collectLatest
+
+@HiltWorker
+class StopwatchWorker @AssistedInject constructor(
+    @Assisted private val stopwatchManager: StopwatchManager,
+    @Assisted private val stopwatchNotificationHelper: StopwatchNotificationHelper,
+    @Assisted ctx: Context,
+    @Assisted params: WorkerParameters,
+) : CoroutineWorker(ctx, params) {
+    override suspend fun doWork(): Result {
+        return try {
+            val foregroundInfo = ForegroundInfo(
+                STOPWATCH_WORKER_NOTIFICATION_ID,
+                stopwatchNotificationHelper.getStopwatchBaseNotification().build(),
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK else 0
+            )
+
+            setForeground(foregroundInfo)
+
+            stopwatchManager.stopwatchState.collectLatest {
+                if (!it.isReset) {
+                    stopwatchNotificationHelper.updateStopwatchWorkerNotification(
+                        time = "${it.hour}:${it.minute}:${it.second}",
+                        isPlaying = it.isPlaying,
+                        lastLapIndex = stopwatchManager.lapTimes.lastIndex,
+                    )
+                }
+            }
+
+            Result.success()
+        } catch (e: CancellationException) {
+            stopwatchNotificationHelper.removeStopwatchNotification()
+            Result.failure()
+        }
+    }
+}
+
+const val STOPWATCH_TAG = "stopwatchTag"
